a1bdff7ad44a2149ead97271e35a46bc3b81e4ae
Aleksander <aleksander.wojnar03@gmail.com>
2023-04-04 13:53:48 +0200

    A

---
+++ Zadanie/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Title</title>
+</head>
+<body>
+    <?php
+    echo "<ul>";
+    echo "<li>";
+    echo "Imię i nazwisko: ".$_POST['name'];
+    echo "</li>";
+    echo "<li>";
+    echo "Twój adres e-mail: ".$_POST['email'];
+    echo "</li>";
+
+?>
+
+</body>
